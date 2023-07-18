ce87a61bf4236dfdcd1037a7f54edf4e557d5dd8
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2023-07-18 21:44:32 +0800

    Improve Popeye UI

--- src/js/tools/popeye.js
+++ src/js/tools/popeye.js
@@ -3,10 +3,21 @@ import { state } from "../store";
 
 let worker;
 let startTime;
+let output;
+let suffix;
+let int;
 
 function stop() {
 	const remain = 1000 - (performance.now() - startTime);
+	state.popeye.output = output;
+	clearInterval(int);
 	setTimeout(() => state.popeye.running = false, Math.max(0, remain));
+}
+
+function animate() {
+	suffix += ".";
+	if(suffix.length == "5") suffix = ".";
+	state.popeye.output = output + "\n" + suffix;
 }
 
 export const Popeye = {
@@ -15,15 +26,17 @@ export const Popeye = {
 			worker = new Worker("modules/py.js");
 			worker.onmessage = event => {
 				if(event.data === null) stop();
-				else state.popeye.output += event.data + "\n";
+				else output += event.data + "\n";
 			};
 		}
-		state.popeye.output = "";
 		const fen = orthodoxForsyth(true);
 		if(!fen) {
 			alert("Only orthodox pieces are supported for now.");
 			return;
 		}
+		output = "";
+		suffix = ".";
+		int = setInterval(animate, 500);
 		const input = `fors ${fen}\n${state.popeye.input}\nopti noboard`
 		startTime = performance.now();
 		state.popeye.running = true;
